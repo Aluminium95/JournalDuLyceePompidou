46221da469a4c75912cb9ac0dcbd42cab45fadd9
Aluminium95 <lopezaliaume@gmail.com>
2011-04-02 13:44:22 +0200

    Ajout de la troisième page ( code identique à la seconde, mais rendu différent, vive nth-child ! )

---
+++ TroisiemePage.php
@@ -0,0 +1,67 @@
+<section>
+	<aside>
+		<article>
+			<h1>Wah un titre de mini article !</h1>
+			<p>Test test test test test test test test</p>
+		</article>
+		<article>
+			<h1>Wah un titre de mini article !</h1>
+			<p>Test test test test test test test test</p>
+		</article>
+		<article>
+			<h1>Wah un titre de mini article !</h1>
+			<ul>
+				<li>Test test test test test</li>
+				<li>Test test test test test</li>
+				<li>Test test test test test</li>
+				<li>Test test test test test</li>
+				<li>Test test test test test</li>
+				<li>Test test test test test</li>
+				<li>Test test test test test</li>
+			</ul>
+		</article>
+	</aside>
+	<article style='dsplay: block;'>
+		<h1>C'est un magnifique article de malade </h1>
+		<div colones='yes'>
+			<h2>Une affaire étrange</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+			Nunc sed egestas tortor. Donec ac lectus vulputate diam eleifend dapibus. 
+			Aliquam quis tellus leo, ac lacinia orci. Phasellus sodales tincidunt risus a molestie. 
+			Aenean eu lectus nec sapien feugiat accumsan. Curabitur imperdiet nisl nec 
+			leo elementum placerat. Aenean tempus tristique lorem, non commodo velit 
+			sodales et. Cras adipiscing adipiscing convallis. Aliquam vel justo velit, 
+			eu dignissim justo. In hac habitasse platea dictumst. Sed nec lobortis dolor. 
+			Ut metus risus, molestie vulputate tristique eu, molestie vel tortor. Donec 
+			eleifend nulla quis metus convallis sagittis. Nulla ultricies, risus quis 
+			hendrerit egestas, neque magna hendrerit nibh, sed aliquet lacus lacus et orci. 
+			Quisque eu leo purus, venenatis interdum sapien. Proin in orci turpis.</p>
+			<h2>Titre suivant</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+			Nunc sed egestas tortor. Donec ac lectus vulputate diam eleifend dapibus. 
+			Aliquam quis tellus leo, ac lacinia orci. Phasellus sodales tincidunt risus a molestie. 
+			Aenean eu lectus nec sapien feugiat accumsan. Curabitur imperdiet nisl nec 
+			leo elementum placerat. Aenean tempus tristique lorem, non commodo velit 
+			sodales et. Cras adipiscing adipiscing convallis. Aliquam vel justo velit, 
+			eu dignissim justo. In hac habitasse platea dictumst.</p>
+		</div>
+		<div role='sign'>Un rédacteur </div>
+	</article>
+	<article>
+		<h1>C'est un magnifique article de malade </h1>
+		<div colones='yes'>
+			<h2>Une affaire étrange</h2>
+			<p>aunriestau eaui, aéodlau livdk'aplod.kaulnd'k alédpoq'k javdok 
+			aeu,klt' apovka' lnotk'aluvdi.k ailte japvoc,jvl'au ekvt ajlov,c aéé
+			okao' japlvok' apzlod.ktan ultde,'jalpk ajto',kjldaotcjklvd ajolv,k</p>
+			<h2>Titre suivant</h2>
+			<p>Paragraphe n°2</p>
+		</div>
+		<div role='img'>
+			<img src='http://www.marine-le-pen.com/wp-content/uploads/2010/11/de087ce8mlp.jpg' />
+			<p>Le front national, comment en est il arrivé là, vers où allons nous ( P.2 )</p>
+		</div>
+		<div colones='yes'>Suite du magnifique article</div>
+		<div role='sign'>Un rédacteur </div>
+	</article>
+</section>
