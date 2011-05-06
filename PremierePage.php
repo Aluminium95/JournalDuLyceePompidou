8071164d11508e178bcf354a4e8c6de9fdec60a2
Aluminium95 <lopezaliaume@gmail.com>
2011-05-06 13:54:15 +0200

    Modification du logo + commentaires dans le code HTML pour facilliter la compréhension

--- PremierePage.php
+++ PremierePage.php
@@ -1,21 +1,21 @@
-<section role='PremierePage'>
+<section role='PremierePage'> <!-- Toutes les pages sont définies par des sections -->
 	<!-- Header -->
 	<header>
-		Le Georges
-		<img src='Images/yeti.svg'/>
+		Le Georges <!-- Titre du journal -->
+		<img src='Images/yeti.png'/> <!-- Logo du journal -->
 	</header>
 	<!-- Bannière info tirage ( css dans head_foot.css ) -->
 		<ul id='infoTirage'>
-			<li>27 mars 2011</li>
-			<li>Alpha 4.0.5</li>
-			<li>Site web / flux atom</li>
+			<li>27 mars 2011</li> <!-- Date -->
+			<li>Alpha 4.0.5</li> <!-- Version ?-->
+			<li>Site web / flux atom</li> <!-- liens -->
 		</ul>
 	<!-- Article Principal -->
 	<article role='Principal'>
 		<h1>Firefox 4 distance Internet Explorer 9</h1>
-		<figure>
-			<img src='http://www.veilleur-strategique.eu/wp-content/uploads/2011/03/firefox-4.jpg' />
-			<figcaption>Image non-contractuelle</figcaption>
+		<figure> <!-- Image sur le côté droit -->
+			<img src='http://www.veilleur-strategique.eu/wp-content/uploads/2011/03/firefox-4.jpg' /> <!-- image -->
+			<figcaption>Image non-contractuelle</figcaption> <!-- Légende -->
 		</figure>
 		<p>
 		Il y a quelques jours, le <b>23 mars</b> dernier, nous vous parlions du 
@@ -25,18 +25,19 @@
 		<u>Explorer 9</u> soit plus du double que la version bêta d’Internet 
 		Explorer 9 (1 million de téléchargements) et 4 fois plus que la 
 		version Release Candidate dans le même laps de temps.</p>
-		<hgroup>
+		<hgroup> <!-- Sous-titres accrocheurs/questions amorces --> 
 			<h2>Comment Microsoft réagit-il ?</h2>
 			<h2>Et chrome dans tout cela ?</h2>
 			<h2>F.F. continuera-t-il sur cette voie ?</h2>
 		</hgroup>
-		<div role='clear'></div>
+		<div role='clear'></div> <!-- Nécessaire après les flottants --> 
 	</article>
 	
-	<menu>
-		<article>
-			<h1>Flambée des prix du gaz : le gouvernement tente de faire marche arrière</h1>
+	<menu> <!-- Menu à la carte pour cette édition du journal :D -->
+		<article> <!-- Présentation d'un article de cette édition -->
+			<h1>Flambée des prix du gaz : le gouvernement tente de faire marche arrière</h1> <!-- Titre de l'article -->
 			<p>Alors qu'une nouvelle hausse de 5,2% vient d'entrer en vigueur, le gouvernement pourrait geler les prix du gaz jusqu'à la présidentielle.</p>
+			<!-- Résumé de l'article -->
 		</article>
 		<article>
 			<h1>Comment Bing, le moteur de recherche de Microsoft, veut contrer Google</h1>
@@ -47,12 +48,13 @@
 			<p>Introduction de cet article</p>
 		</article>
 	</menu>
-	<article role='Edito'>
-		<h1>Editorial</h1>
+	<article role='Edito'> <!-- Court éditorial en première page --> 
+		<h1>Editorial</h1> <!-- Titre de l'édito -->
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
 		Nunc sed egestas tortor. Donec ac lectus vulputate diam eleifend dapibus. 
 		Aliquam quis tellus leo, ac lacinia orci. Phasellus sodales tincidunt risus a molestie. 
-		Aenean eu lectus nec sapien <em>feugiat accumsan</em>. Curabitur imperdiet nisl nec 
+		Aenean eu lectus nec sapien <em>feugiat accumsan</em>. <!-- Mise en italique --> 
+		Curabitur imperdiet nisl nec 
 		leo elementum placerat. Aenean tempus tristique lorem, non commodo velit 
 		sodales et. Cras adipiscing adipiscing convallis. Aliquam vel justo velit, 
 		eu dignissim justo. In hac habitasse platea dictumst. Sed nec lobortis dolor. 
@@ -60,9 +62,9 @@
 		eleifend nulla quis metus convallis sagittis. Nulla ultricies, risus quis 
 		hendrerit egestas, neque magna hendrerit nibh, sed aliquet lacus lacus et orci. 
 		Quisque eu leo purus, venenatis interdum sapien. Proin in orci turpis.</p>
-		<footer>Toute l'équipe de rédaction</footer>
+		<footer>Toute l'équipe de rédaction</footer> <!-- signature -->
 	</article>
-	<footer role='footpage'>
+	<footer role='footpage'> <!-- Footer du journal, pour mettre quelque chose si besoin -->
 		Ceci est une maquette, une version Alpha ( 3.1.3 )
 	</footer>
 </section>
--- SecondePage.php
+++ SecondePage.php
@@ -1,33 +1,33 @@
-<section>
-	<aside>
-		<article>
-			<h1>Musique</h1>
+<section> <!-- Après la première page, plus besoin de définir le numéro -->
+	<aside> <!-- Barre latérale pour les minis-articles --> 
+		<article> <!-- Un article -->
+			<h1>Musique</h1> <!-- Titre de l'article -->
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-		Nunc sed egestas tortor. Donec ac lectus vulputate diam eleifend dapibus. 
-		Aliquam quis tellus leo, ac lacinia orci. Phasellus sodales tincidunt risus a molestie. 
-		Aenean eu lectus nec sapien feugiat accumsan. Curabitur imperdiet nisl nec 
-		leo elementum placerat. Aenean tempus tristique lorem, non commodo velit 
-		sodales et. Cras adipiscing adipiscing convallis. Aliquam vel justo velit, 
-		eu dignissim justo. In hac habitasse platea dictumst. Sed nec lobortis dolor. 
-		Ut metus risus, molestie vulputate tristique eu, molestie vel tortor. Donec 
-		eleifend nulla quis metus convallis sagittis. Nulla ultricies, risus quis 
-		hendrerit egestas, neque magna hendrerit nibh, sed aliquet lacus lacus et orci. 
-		Quisque eu leo purus, venenatis interdum sapien. Proin in orci turpis.</p>
-		<footer>Toute l'équipe de rédaction</footer></p>
-		<div role='clear'></div>
+			Nunc sed egestas tortor. Donec ac lectus vulputate diam eleifend dapibus. 
+			Aliquam quis tellus leo, ac lacinia orci. Phasellus sodales tincidunt risus a molestie. 
+			Aenean eu lectus nec sapien feugiat accumsan. Curabitur imperdiet nisl nec 
+			leo elementum placerat. Aenean tempus tristique lorem, non commodo velit 
+			sodales et. Cras adipiscing adipiscing convallis. Aliquam vel justo velit, 
+			eu dignissim justo. In hac habitasse platea dictumst. Sed nec lobortis dolor. 
+			Ut metus risus, molestie vulputate tristique eu, molestie vel tortor. Donec 
+			eleifend nulla quis metus convallis sagittis. Nulla ultricies, risus quis 
+			hendrerit egestas, neque magna hendrerit nibh, sed aliquet lacus lacus et orci. 
+			Quisque eu leo purus, venenatis interdum sapien. Proin in orci turpis.</p>
+			<footer>Toute l'équipe de rédaction</footer> <!-- signature -->
+			<div role='clear'></div>
 		</article>
 		<article>
 			<h1>Film</h1>
 			<p>Lorem <mark>Test un deux trois</mark> ipsum dolor sit amet, consectetur adipiscing elit. 
-		Nunc sed egestas tortor. Donec ac lectus vulputate diam eleifend dapibus. 
-		Aliquam quis tellus leo, ac lacinia orci. Phasellus sodales tincidunt risus a molestie. 
-		Aenean eu lectus nec sapien feugiat accumsan.Aenean tempus tristique lorem, non commodo velit 
-		sodales et. Cras adipiscing adipiscing convallis.</p>
+			Nunc sed egestas tortor. Donec ac lectus vulputate diam eleifend dapibus. 
+			Aliquam quis tellus leo, ac lacinia orci. Phasellus sodales tincidunt risus a molestie. 
+			Aenean eu lectus nec sapien feugiat accumsan.Aenean tempus tristique lorem, non commodo velit 
+			sodales et. Cras adipiscing adipiscing convallis.</p>
 		</article>
 		<article>
 			<h1>Wah un titre de mini article !</h1>
-			<ul>
-				<li>Test test test test test</li>
+			<ul> <!-- une liste à puces -->
+				<li>Test test test test test</li> <!-- Un item de la liste -->
 				<li>Test test test test test</li>
 				<li>Test test test test test</li>
 				<li>Test test test test test</li>
@@ -37,22 +37,23 @@
 			</ul>
 		</article>
 	</aside>
-	<article>
+	
+	<article><!-- Un article « central » -->
 		<h1>Flambée des prix du gaz : le gouvernement tente de faire marche arrière</h1>
-		<div colones='yes'>
+		<div colones='yes'> <!-- colones='yes' permet la mise en colone du texte -->
 			<p>Alors qu'une nouvelle hausse de 5,2% vient d'entrer en vigueur, le gouvernement pourrait geler les prix du gaz jusqu'à la présidentielle.
 			La nouvelle avait fait grincer les dents de plus d'un foyer. La semaine dernière, on apprenait que EDF avait soumis à l'État un plan de hausse des tarifs du gaz sur 5 ans.
 			Résultat : une hausse des prix de 5 à 6,5% tous les ans... L'État, détenteur à 32% de GDF Suez serait contre cette augmentation.</p>
-			<figure>
-				<img src='http://medias.lepost.fr/ill/2010/01/20/h-4-1898041-1264009770.jpg'/>
-				<figcaption>Une image de multi-prise ( sans rapport avec le gaz )</figcaption>
+			<figure> <!-- Une figure --> 
+				<img src='http://medias.lepost.fr/ill/2010/01/20/h-4-1898041-1264009770.jpg'/> <!-- image -->
+				<figcaption>Une image de multi-prise ( sans rapport avec le gaz )</figcaption> <!-- légende -->
 			</figure>
-			<h2>Une nouvelle hausse de 5,2% vient d'entrer en vigueur</h2>
+			<h2>Une nouvelle hausse de 5,2% vient d'entrer en vigueur</h2> <!-- sous partie -->
 			<p>Une nouvelle qui a suscité une vague de contestation auprès des Français. Au point que le gouvernement envisage un gel du prix du gaz jusqu'à la présidentielle.
 			Vendredi après-midi une réunion interministérielle s'est tenue à Matignon. 
 			Elle rassemblait le Premier ministre François Fillon, la ministre de l'Économie 
 			Christine Lagarde, son homologue de l'Énergie Éric Besson et le secrétaire d'État 
-			à la Consommation, Frédéric Lefebvre.
+			à la Consommation, Frédéric Lefebvre.</p>
 
 			<p>Au menu : la question des prix de l'énergie et notamment celle de la formule 
 			permettant de fixer les prix du gaz, rapporte Le Parisien. Toutefois Matignon 
@@ -73,7 +74,7 @@
 			<p>La hausse des prix du gaz s'inscrit dans un contexte de renchérissement des 
 			prix de l'énergie (+ 15 % en un an) des prix du pétrole brut.</p>
 		</div>
-		<footer>Un rédacteur </footer>
+		<footer>Un rédacteur </footer> <!-- signature -->
 	</article>
 	<article>
 		<h1>Un super article</h1>
