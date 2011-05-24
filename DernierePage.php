1ea4e84155fbd941302175718e091a606f43f6c7
Aluminium95 <lopezaliaume@gmail.com>
2011-05-24 20:33:04 +0200

    Modification du layout de la dernière page

--- DernierePage.php
+++ DernierePage.php
@@ -1,32 +1,36 @@
+<?php include("sudoku.php"); ?>
 <section>
 	<article>
 		<h1>Sudoku</h1>
-		<div>
-			<table style='margin: auto;'> <!-- Conteneur du sudoku -->
-				<?php include("sudoku.php"); ?>
+		<div style='padding-left: 100px;'>
+			<table style='float: left'> <!-- Conteneur du sudoku -->
+				<?php aff(); ?>
+			</table>
+			<div style='height: 280px;float: left;border:1px solid black'></div>
+			<table> <!-- Conteneur du sudoku -->
+				<?php aff(); ?>
 			</table>
 		</div>
 	</article>
 	<article>
-		<h1>Facebook is watching you™</h1>
+		<h1>Le cartable numérique</h1>
 		<div colones='yes'>
-		<p>Lundi, 18h. Je rentre du lycée. Porte fermée, sac jeté sur le sol, un bonjour à la cantonade, et hop ! Plus personne. Non, non je n’ai pas disparu. C’est facile de me trouver à cette heure-là : devant l’ordi. Allez, vous avez beau dire, je suis sûre que vous le faîte vous aussi. En vérité, je fais partie des 82% de lycéens qui se connectent sur Facebook directement après les cours.
+			<p>
+				Le projet de la région « école numérique » se prépare, et pour cause, l’année prochaine, les secondes auront chacun la chance de recevoir un ordinateur portable, un don de la région. Il sera doté d’un écran 13", d’une bonne autonomie, du logiciel Windows et de la suite bureautique OpenOffice pour un poids (plume ?) : inférieur à 2,3kg.
 
-Facebook. On a beau avoir des centaines d’amis, <em>c’est quand même lui le seul que l’on a tous en commun</em>. Mais quand on y réfléchit, quelle est notre motivation, nous qui nous sommes inscrits sur le plus grand réseau social au monde ?
+Du côté du lycée, le service informatique a déjà installé des bornes Wifi aux quatre coins de l’établissement : au CDI, à la cafeteria, dans la cour… De plus, afin de faciliter la communication entre lycéens et professeurs, le rectorat va mettre en place un environnement de travail numérique, où chacun pourra consulter son agenda, ses cours ou autre, et où les profs pourrons publier des cours.
 
-Premier argument : garder le contact avec des amis qu’on ne voit pas tous les jours. D’accord, ça se tient. On voit leurs actus, leurs photos, oh tiens ! Le nouveau petit copain. Mais honnêtement, combien d’entre vous leur parlent ? Que ceux qui l’on déjà fait se fassent connaître (façon de parler, ne levez pas un bras dans la cafet’, ce serait ridicule). On donne toujours cette excuse quand un anti-Facebook nous pose la question, mais en fait, c’est pas la réalité, et ça, tout le monde le sait.
+Bien sûr, dites-vous. Mais quelle est l’utilité d’un tel investissement au lycée ? C’est justement la question que l’on a posé à de multiples personnes.
 
-Deuxième argument : Facebook permet de rassembler beaucoup de gens. Validé. On a bien vu ce que ça a donné avec les blocus, et là, c’est pas négligeable.
+L’avis des enseignants est plutôt positif, mais tout de même mitigé. Un professeur de mathématiques pense que cela lui permettra de faire des « travaux pratiques », avec l’ordinateur, dans la dynamique de son cours : il ne sera plus obligé d’attendre qu’il ait une salle informatique pour consacrer une heure entière à un TP. Il déplore par contre l’absence des manuels numérique et se demande ce qu’il fera si un élève casse son ordinateur, par exemple. Un autre professeur revient sur ces problèmes, qui sont pour lui analogues à ceux qu’il rencontre habituellement tel que la perte malencontreuse d’un cahier… Puis, un dernier professeur approuve l’usage de l’outil informatique pendant ses cours, ce qui lui permettra de faire des simulations, entrer autre.
 
-Troisième argument : <em>Facebook rapproche les gens et crée des liens</em>. Oui, enfin… Des liens temporaires dans ce cas-là. Qui parle encore à ses camarades de seconde ? (Les secondes de cette année ne sont pas concernés bien sûr, sinon c’est de la triche). C’est vrai que Facebook permet de fuir la solitude et de rester connectés avec ses « amis » 24h sur 24, mais enfin, quelle est l’utilité de savoir tout sur tout le monde ? Il y a des choses qui ne sont pas toujours bonnes à savoir.
+Les élève pensent globalement que d’équiper les secondes d’ordinateurs est une bonne initiative, souffrant de quelques défauts. Prenons le poids de l’ordinateur + l’absence de manuel numérique. Après un moment de réflexion on s’aperçoit que… le poids du sac d’un lycéen va augmenter ! Heureusement, cette situation va évoluer avec le temps.
 
-Bon, pour ceux qui ont noté le sous-titre (+1), explication. Bien sûr je suis certaine que vous le savez, mais on ne peut pas parler de Facebook sans parler de tout ce qui ne va pas, et croyez bien que j’en suis navrée. S’il n’y avait aucun problème, ça m’aurait fait moins de boulot pendant les vacances. Bien sûr, vous avez tous entendu parler des failles de sécurité de Facebook (je l’espère pour vous). Hé oui, la confidentialité, ça vous dit quelque chose ? 73% des utilisateurs de Facebook ne pensent partager leurs contenus personnels qu’avec des amis mais seulement 42% ont réglé leurs paramètres de confidentialité sur « amis uniquement ». Le problème, c’est que 52% des utilisateurs publient des informations susceptibles de mettre en danger leur vie privée. Un sondage a montré que 40% des étudiants publient leur numéro de téléphone et 51% publient leur adresse sur leur profil. Inconscients vous avez dits ? Allez, je vous laisse digérer tous ces chiffres. Une dernière information cependant : tout le monde a accès à vos informations si vous ne modifiez pas vos paramètres. Et quand je dis tout le monde, c’est tout le monde. Même les applications idiotes, comme les anciens quiz à la mode au collège du genre « Quelle couleur es-tu ? » (Avec une faute généralement) ont accès à toutes nos infos personnelles. Vous serez étonnés de voir tous les tests bidons que vous avez bien pu faire. Moi par exemple, j’ai eu droit à « Quel Schtroumpf es-tu ? » (avec et sans la faute) et « Are YOU a muffin ? ». Oui, oui, on est bête quand on a 13 ans. Le problème ? Avec le temps, vous oubliez la réponse, et même le fait que vous l’ayez fait. Sauf que les applications ont encore accès à toutes vos informations.
+Et les documentalistes du lycée pensent que l’outil informatique est très intéressant pédagogiquement, et que c’est important que cet outil soit donné, pour l’égalité des chances. Elles  remarquent qu’il faut que (certains ?) lycéens arrêtent de voir un ordinateur uniquement comme un jouet, mais aussi comme un outil pour leurs études.
 
-Résultat ? <mark>250 applications avaient accès à mon profil</mark>. Croyez-moi, quand on les supprime une à une (pas le choix), on les sent passer.
-
-De plus, Facebook entraîne une dépendance. Oui sans rire. Quand on se connecte directement en rentrant du lycée, ou quand, en prenant une photo, on pense à l’instant au nombre de personnes qui vont « l’aimer », c’est fichu… Paix à nos âmes ! Autant vous prévenir aussi : les publicités sont visées et toute information que vous postez peut être vendue. Et oui, les photos de la fête de Laura samedi soir, dès dimanche matin, elles font partie du domaine public et peuvent se retrouver dans une pub pour Coca. Dur. Oh, maintenant qu’on parle de fête. Imaginez-vous dans trois ou quatre ans, quand vous postulerez pour un super poste. Je ne suis pas sûre que les photos de vous avec une tête pas très fraîche et une bouteille de bière/vodka/malibu à la main (rayez la mention inutile) fassent vraiment plaisir à votre ex-futur employeur. Idem pour les statuts qui expriment un peu trop vos avis politiques par exemple. <strong>La vérité, c’est que Facebook n’est pas notre ami</strong>. Ou en tout cas, il le devient à partir du moment où nous prenons les commandes. Après tout ça, dur à croire que (oui une dernière statistique) 35% des jeunes n’ont jamais réfléchit aux problèmes de sécurité posé par les applications Facebook, ce qui est énorme.
-
-Alors ce soir, en rentrant chez vous, quand vous vous connecterez, cliquez pour moi sur le bouton « Compte – Paramètres de confidentialité. » Et maintenant, vous pourrez commenter vos photos beaucoup plus tranquillement.</p>
-	</div>
+Cette nouveauté est donc globalement perçue comme étant une bonne initiative, mais ayant malheureusement des défauts non négligeable, qui, espérons-le, se résoudront avec le temps. Il est normal de rencontrer des problèmes au lancement qui n’est en somme qu’un premier pas.
+			</p>
+		</div>
+		<footer>F.L. & Alu95</footer>
 	</article>
 </section>
