ac81ba1e52656dec902a3257b0a5412aac6f5b6e
Aluminium95 <lopezaliaume@gmail.com>
2011-05-24 20:33:30 +0200

    Update générale !

--- PremierePage.php
+++ PremierePage.php
@@ -6,7 +6,7 @@
 	</header>
 	<!-- Bannière info tirage ( css dans head_foot.css ) -->
 		<ul id='infoTirage'>
-			<li>27 mars 2011</li> <!-- Date -->
+			<li><?php echo(date("j.n.Y")); ?></li> <!-- Date -->
 			<li>Alpha 4.0.5</li> <!-- Version ?-->
 			<li>Site web / flux atom</li> <!-- liens -->
 		</ul>
@@ -58,18 +58,9 @@
 	</menu>
 	<article role='Edito'> <!-- Court éditorial en première page --> 
 		<h1>Editorial</h1> <!-- Titre de l'édito -->
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-		Nunc sed egestas tortor. Donec ac lectus vulputate diam eleifend dapibus. 
-		Aliquam quis tellus leo, ac lacinia orci. Phasellus sodales tincidunt risus a molestie. 
-		Aenean eu lectus nec sapien <em>feugiat accumsan</em>. <!-- Mise en italique --> 
-		Curabitur imperdiet nisl nec 
-		leo elementum placerat. Aenean tempus tristique lorem, non commodo velit 
-		sodales et. Cras adipiscing adipiscing convallis. Aliquam vel justo velit, 
-		eu dignissim justo. In hac habitasse platea dictumst. Sed nec lobortis dolor. 
-		Ut metus risus, molestie vulputate tristique eu, molestie vel tortor. Donec 
-		eleifend nulla quis metus convallis sagittis. Nulla ultricies, risus quis 
-		hendrerit egestas, neque magna hendrerit nibh, sed aliquet lacus lacus et orci. 
-		Quisque eu leo purus, venenatis interdum sapien. Proin in orci turpis.</p>
+		<p>« Le Georges, mais c’est nul comme titre. Ha. Haha. Ha. » Telle est hélas la réaction que beaucoup (trop) d’entre vous ont dû avoir en attrapant un exemplaire de ce premier numéro (pourtant si prometteur…). D’aucuns diront même que l’on ne peut carrément pas faire pire. Je pense que ces inconscients n’ont pas vu mon carnet d’idée… Bref, revenons à notre titre figurant quelques centimètres plus haut. Outre le jeu de mot facile et commercial (pour lequel une célèbre marque de café devrait nous payer des millions) qui l’accompagne, le choix de ce prénom aux résonnances aristocratiques a une ténébreuse origine. En effet, un soir, sortant de deux heures atroces (à la limite du respect des droits de l’homme) de cours avec Mr ********, professeur de ****** je croisai deux hommes à l’allure sombre. Il pleuvait dans une nuit tombant trop tôt pour une région dite « ensoleillée ». Ils étaient là, sous un réverbère.  L’un portait un pardessus noir tandis que je voyais saillir un revolver sous le blouson de l’autre. Ils semblaient totalement obnubilés par leur conversation ; à la fois fascinés et terrorisés par les mots qu’ils prononçaient. Alors que j’arrivais à leur hauteur, une phrase s’envola jusqu’à mes oreilles. Tout s’arrêta. Mes jambes continuèrent de se placer l’une devant l’autre mais il m’est impossible de me souvenir du reste du trajet jusqu’à chez moi… Et je vous assure que quand vous connaitrez la nouvelle que j’appris ce jour là, vous ne pourrez que comprendre quel fut mon état de choc. J’espère que vous êtes confortablement installé, et que l’infirmerie est capable de gérer la vague d’évanouissement que la prochaine phrase va provoquer. Mes amis, j’ai appris ce soir là que le « G. » discrètement placé devant « pompidou », le nom de notre…euh… très cher ( ?) lycée, n’est pas qu’une simple lubie de proviseur : c’est l’initiale d’un prénom, celui d’un ancien président de la Vème république parait-il. Ce prénom, c’est Georges. Ne pouvant garder pour nous une nouvelle d’une telle ampleur, nous avons décidé d’en faire le titre de notre journal.
+
+Voilà pourquoi le journal a comme titre un prénom moche.</p>
 		<footer>Toute l'équipe de rédaction</footer> <!-- signature -->
 	</article>
 	<footer role='footpage'> <!-- Footer du journal, pour mettre quelque chose si besoin -->
