7386e3bdaf19f1453db22723d022c2c2947c239a
Aluminium95 <lopezaliaume@gmail.com>
2011-05-26 22:11:09 +0200

    Modification des mises en page des articles ( respect des paragraphes )

--- Pages/4/Articles/Normaux.php
+++ Pages/4/Articles/Normaux.php
@@ -8,17 +8,17 @@
 			<img src='Images/EcoleNumerique.jpg'/>
 		</figure>
 		<p>
-Du côté du lycée, le service informatique a déjà installé des bornes Wifi aux quatre coins de l’établissement : au CDI, à la cafeteria, dans la cour… De plus, afin de faciliter la communication entre lycéens et professeurs, le rectorat va mettre en place un environnement de travail numérique, où chacun pourra consulter son agenda, ses cours ou autre, et où les profs pourrons publier des cours.
+<p>Du côté du lycée, le service informatique a déjà installé des bornes Wifi aux quatre coins de l’établissement : au CDI, à la cafeteria, dans la cour… De plus, afin de faciliter la communication entre lycéens et professeurs, le rectorat va mettre en place un environnement de travail numérique, où chacun pourra consulter son agenda, ses cours ou autre, et où les profs pourrons publier des cours.</p>
 
-Bien sûr, dites-vous. Mais quelle est l’utilité d’un tel investissement au lycée ? C’est justement la question que l’on a posé à de multiples personnes.
+<p>Bien sûr, dites-vous. Mais quelle est l’utilité d’un tel investissement au lycée ? C’est justement la question que l’on a posé à de multiples personnes.</p>
 
-L’avis des enseignants est plutôt positif, mais tout de même mitigé. Un professeur de mathématiques pense que cela lui permettra de faire des « travaux pratiques », avec l’ordinateur, dans la dynamique de son cours : il ne sera plus obligé d’attendre qu’il ait une salle informatique pour consacrer une heure entière à un TP. Il déplore par contre l’absence des manuels numérique et se demande ce qu’il fera si un élève casse son ordinateur, par exemple. Un autre professeur revient sur ces problèmes, qui sont pour lui analogues à ceux qu’il rencontre habituellement tel que la perte malencontreuse d’un cahier… Puis, un dernier professeur approuve l’usage de l’outil informatique pendant ses cours, ce qui lui permettra de faire des simulations, entrer autre.
+<p>L’avis des enseignants est plutôt positif, mais tout de même mitigé. Un professeur de mathématiques pense que cela lui permettra de faire des « travaux pratiques », avec l’ordinateur, dans la dynamique de son cours : il ne sera plus obligé d’attendre qu’il ait une salle informatique pour consacrer une heure entière à un TP. Il déplore par contre l’absence des manuels numérique et se demande ce qu’il fera si un élève casse son ordinateur, par exemple. Un autre professeur revient sur ces problèmes, qui sont pour lui analogues à ceux qu’il rencontre habituellement tel que la perte malencontreuse d’un cahier… Puis, un dernier professeur approuve l’usage de l’outil informatique pendant ses cours, ce qui lui permettra de faire des simulations, entrer autre.</p>
 
-Les élève pensent globalement que d’équiper les secondes d’ordinateurs est une bonne initiative, souffrant de quelques défauts. Prenons le poids de l’ordinateur + l’absence de manuel numérique. Après un moment de réflexion on s’aperçoit que… le poids du sac d’un lycéen va augmenter ! Heureusement, cette situation va évoluer avec le temps.
+<p>Les élève pensent globalement que d’équiper les secondes d’ordinateurs est une bonne initiative, souffrant de quelques défauts. Prenons le poids de l’ordinateur + l’absence de manuel numérique. Après un moment de réflexion on s’aperçoit que… le poids du sac d’un lycéen va augmenter ! Heureusement, cette situation va évoluer avec le temps.</p>
 
-Et les documentalistes du lycée pensent que l’outil informatique est très intéressant pédagogiquement, et que c’est important que cet outil soit donné, pour l’égalité des chances. Elles  remarquent qu’il faut que (certains ?) lycéens arrêtent de voir un ordinateur uniquement comme un jouet, mais aussi comme un outil pour leurs études.
+<p>Et les documentalistes du lycée pensent que l’outil informatique est très intéressant pédagogiquement, et que c’est important que cet outil soit donné, pour l’égalité des chances. Elles  remarquent qu’il faut que (certains ?) lycéens arrêtent de voir un ordinateur uniquement comme un jouet, mais aussi comme un outil pour leurs études.</p>
 
-Cette nouveauté est donc globalement perçue comme étant une bonne initiative, mais ayant malheureusement des défauts non négligeable, qui, espérons-le, se résoudront avec le temps. Il est normal de rencontrer des problèmes au lancement qui n’est en somme qu’un premier pas.
+<p>Cette nouveauté est donc globalement perçue comme étant une bonne initiative, mais ayant malheureusement des défauts non négligeable, qui, espérons-le, se résoudront avec le temps. Il est normal de rencontrer des problèmes au lancement qui n’est en somme qu’un premier pas.
 		</p>
 	</div>
 	<footer>F.L. & Alu95</footer>
@@ -34,9 +34,9 @@ Cette nouveauté est donc globalement perçue comme étant une bonne initiative,
 		<figcaption>Mise à l'index</figcaption>
 	</figure>
 	<p>
-Un autre petit mot, sur le site du journal cette fois. Étant donné que nous n'avons pas des moyens illimités en impression, il nous a semblé judicieux de mettre en place un site ( http://journalpompidou.free.fr ) sur lequel vous pourriez avoir accès à tous les articles et les commenter à tout moment ! C'est aussi sur ce site que se font les sondages ( pour des raisons pratiques que vous pouvez parfaitement comprendre, ramasser 500 coupons de vote et les trier pour ensuite faire des statistiques n'est pas … idéal ). Nous vous incitons vivement à visiter le-dit site web et nous faire part de vos impression !
+Un autre petit mot, sur le site du journal cette fois. Étant donné que nous n'avons pas des moyens illimités en impression, il nous a semblé judicieux de mettre en place un site ( http://journalpompidou.free.fr ) sur lequel vous pourriez avoir accès à tous les articles et les commenter à tout moment ! C'est aussi sur ce site que se font les sondages ( pour des raisons pratiques que vous pouvez parfaitement comprendre, ramasser 500 coupons de vote et les trier pour ensuite faire des statistiques n'est pas … idéal ). Nous vous incitons vivement à visiter le-dit site web et nous faire part de vos impression !</p>
 
-Merci d'avoir lu cet article d'information, on se retrouve au prochain numéro ( sauf si vous me détestez profondément, et haïssez mon écriture archaïque et barbare ).
+<p>Merci d'avoir lu cet article d'information, on se retrouve au prochain numéro ( sauf si vous me détestez profondément, et haïssez mon écriture archaïque et barbare ).
 	</p>
 	</div>
 	<footer>Alu95</footer>
